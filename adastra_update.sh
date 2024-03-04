f087b6c5d11480475aa4facf02111816141c8bfd
Vladislav A. Nachatoi <135841257+nachatoi@users.noreply.github.com>
2024-03-04 21:23:46 +0300

    Add files via upload

---
+++ adastra_update.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+file_list='/home/subpolare/adastra/bill_cipher_TFs.txt'
+genome='/home/subpolare/genome/GRCh38.primary_assembly.genome.fa'
+output='/home/subpolare/adastra/adastra_pwm_0.01.tsv'
+p_value=0.001
+threads=10
+discr=1000
+
+echo -e "Factor\tAdastra_concordance\tConcordance\tAdastra_discordance\tDiscordance\tAdastra_hit\tHit\tAdastra_ho_hit\tNo_hit\t0\t1\t2\t3" > adastra_pwm_0.tsv
+echo -e "Factor\tAdastra_concordance\tConcordance\tAdastra_discordance\tDiscordance\tAdastra_hit\tHit\tAdastra_ho_hit\tNo_hit\t0\t1\t2\t3" > adastra_pwm_1.tsv
+echo -e "Factor\tAdastra_concordance\tConcordance\tAdastra_discordance\tDiscordance\tAdastra_hit\tHit\tAdastra_ho_hit\tNo_hit\t0\t1\t2\t3" > adastra_pwm_2.tsv
+echo -e "Factor\tAdastra_concordance\tConcordance\tAdastra_discordance\tDiscordance\tAdastra_hit\tHit\tAdastra_ho_hit\tNo_hit\t0\t1\t2\t3" > adastra_pwm_3.tsv
+
+mkdir pwm_results_0
+mkdir pwm_results_1
+mkdir pwm_results_2
+mkdir pwm_results_3
+
+process_file() {
+    tf=$1
+    index=$2
+    discr=$3
+    p_value=$4
+    genome=$5
+    factor=$(basename ${tf} | cut -f1 -d '.' | cut -f1 -d '_')
+
+    scripts/asb.py ${tf} 0.05 ${factor}
+    scripts/make_snps_list.sh ${factor}.filtered ${genome} > ${factor}.snps
+
+    java -cp scripts/ape-3.0.6.jar ru.autosome.perfectosape.SNPScan hocomoco/v12/pwm/${factor}.H12RSNP.${index}.*.pwm ${factor}.snps --single-motif -F 1 -P 1 -d ${discr} > pwm_results_${index}/${factor}.perfectos
+
+    conc_adastra=$(grep -E 'Concordant' ${factor}.filtered | wc -l)
+    disc_adastra=$(grep -E 'Discordant' ${factor}.filtered | wc -l)
+    nohit_adastra=$(grep -E 'No Hit' ${factor}.filtered | wc -l)
+    scripts/concordance.py pwm_results_${index}/${factor}.perfectos ${tf} ${conc_adastra} ${disc_adastra} ${nohit_adastra} ${factor} ${p_value} >> adastra_pwm_${index}.tsv
+}
+
+export -f process_file
+
+parallel -j $threads process_file ::: $(cat ${file_list}) ::: 0 ::: ${discr} ::: ${p_value} ::: ${genome}
+parallel -j $threads process_file ::: $(cat ${file_list}) ::: 1 ::: ${discr} ::: ${p_value} ::: ${genome}
+parallel -j $threads process_file ::: $(cat ${file_list}) ::: 2 ::: ${discr} ::: ${p_value} ::: ${genome}
+parallel -j $threads process_file ::: $(cat ${file_list}) ::: 3 ::: ${discr} ::: ${p_value} ::: ${genome}
+
+scripts/adastra_pwm.py ${output}
+
+rm ${factor}.filtered ${factor}.snps
+find pwm_results_${index} -maxdepth 1 -type f -size 0 -delete
---
+++ make_snps_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+  
+input=$1
+genome='/home/subpolare/genome/GRCh38.primary_assembly.genome.fa'
+
+while IFS=$'\t' read -r chr pos ID ref alt repeat_type n_peak_calls n_peak_callers mean_BAD mean_SNP_per_segment n_aggregated total_cover es_mean_ref es_mean_alt fdrp_bh_ref fdrp_bh_alt motif_log_pref motif_log_palt motif_fc motif_pos motif_orient motif_conc; do
+        left_sequence=$(bedtools getfasta -fi "${genome}" -bed <(echo -e "${chr}\t$((pos-30))\t$((pos-1))") -name | grep -v ">")
+        right_sequence=$(bedtools getfasta -fi "${genome}" -bed <(echo -e "${chr}\t$((pos))\t$((pos+30))") -name | grep -v ">")
+
+	echo -e "${ID}\t${left_sequence}[${ref}/${alt}]${right_sequence}"
+done < $input | tail -n +2
