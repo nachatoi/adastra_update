006efa172e72e8995279e80aa48d5b011877e907
Vladislav A. Nachatoi <135841257+nachatoi@users.noreply.github.com>
2024-03-08 12:57:52 +0300

    Update make_snps_list.sh

--- make_snps_list.sh
+++ make_snps_list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
 input=$1
-genome='/home/subpolare/genome/GRCh38.primary_assembly.genome.fa'
+genome=$2
 
 while IFS=$'\t' read -r chr pos ID ref alt repeat_type n_peak_calls n_peak_callers mean_BAD mean_SNP_per_segment n_aggregated total_cover es_mean_ref es_mean_alt fdrp_bh_ref fdrp_bh_alt motif_log_pref motif_log_palt motif_fc motif_pos motif_orient motif_conc; do
         left_sequence=$(bedtools getfasta -fi "${genome}" -bed <(echo -e "${chr}\t$((pos-30))\t$((pos-1))") -name | grep -v ">")
